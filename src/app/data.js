758b1c9c71d698fa6a634b1c101eceb2a800171f
yuxunn <teeyuxun@gmail.com>
2024-08-14 16:45:21 +0800

    backend-logic

--- src/app/data.js
+++ src/app/data.js
@@ -3,97 +3,97 @@ export const quiz = {
     {
       question: "1. youre on board the bus to the latest and greatest, most Instrgram-worthy amusement park in town, veggie land! What is the first ride you will take when you reach veggie land?",
       answers: [
-        { text: "merry-go-round, its nice n peaceful", category: "A" },
-        { text: "Im just here to take pictures with the veggies…", category: "B" },
-        { text: "cauliflower coaster!!!! its rumoured to be gravity defying and exhillerating!", category: "D" },
+        { text: "merry-go-round, its nice n peaceful", category: "E" },
+        { text: "Im just here to take pictures with the veggies…", category: "F" },
+        { text: "cauliflower coaster!!!! its rumoured to be gravity defying and exhillerating!", category: "P" },
       ],
     },
     {
       question: "As you were starting to doze off, the bus screeches to a halt!",
       answers: [
-        { text: "Who dare wake me up from my beauty sleep?!", category: "A" },
-        { text: "Oh no … goes back to sleep", category: "B" },
-        { text: "what is happening? looks around frantically", category: "C" }
+        { text: "Who dare wake me up from my beauty sleep?!", category: "N" },
+        { text: "Oh no … goes back to sleep", category: "I" },
+        { text: "what is happening? looks around frantically", category: "J" }
       ],
     },
     {
       question: "You look out of the window and saw the bus hanging at the edge of the cliff!",
       answers: [
-        { text: "Immediately stands up and try to find a solution", category: "A" },
-        { text: "I have surrended to my fate…", category: "B" },
-        { text: "Looks around to see if anyone is reacting.", category: "C" }
+        { text: "Immediately stands up and try to find a solution", category: "J" },
+        { text: "I have surrended to my fate…", category: "F" },
+        { text: "Looks around to see if anyone is reacting.", category: "S" }
       ],
     },
     {
       question: "AHHHHHHHHH The bus is free falling! You hear a loud splash as the bus sinks to the ocean floor. Water enters your bus, but by some force of magic, you survived.. and gained the ability to breathe underwater?! What will you do next?",
       answers: [
-        { text: "Explore the deep blue sea", category: "A" },
-        { text: "i need a moment to process…", category: "B" },
-        { text: "ask the nearby fishes for help ", category: "C" }
+        { text: "Explore the deep blue sea", category: "P" },
+        { text: "i need a moment to process…", category: "J" },
+        { text: "ask the nearby fishes for help ", category: "E" }
       ],
     },
     {
       question: "You step out into the cold, unforgiving darkness. As you wade along, suddenly, you see a speck of light in the distance.",
       answers: [
-        { text: "this could be my only hope out of here! ", category: "A" },
-        { text: "walks towards the light without hesitation", category: "B" },
-        { text: "ahhhhhhhhh!!!! is that a shark with glowing fangs?????", category: "C" }
+        { text: "this could be my only hope out of here! ", category: "T" },
+        { text: "walks towards the light without hesitation", category: "I" },
+        { text: "ahhhhhhhhh!!!! is that a shark with glowing fangs?????", category: "S" }
       ],
     },
     {
       question: "The light starts moving towards you. Turns out its an anglerfish! As it comes to you, the fish moves it mouth. “you look lost, my dear” (the fish should b mewing)",
       answers: [
-        { text: "why is it kinda… good-looking?", category: "A" },
-        { text: "it can talk??? why is a fish talking to me?!", category: "B" },
-        { text: "yes im lost help me", category: "C" }
+        { text: "why is it kinda… good-looking?", category: "N" },
+        { text: "it can talk??? why is a fish talking to me?!", category: "S" },
+        { text: "yes im lost help me", category: "F" }
       ],
     },
     {
       question: "Do not worry, i am here to help you. I am King George, mighty ruler of the seven seas. I will bring you to my kingdom. You will be safe with me.” the fish says. As it talks, you get a glimpse of its sharp, long teeth. ",
       answers: [
-        { text: "I trust him with my life", category: "A" },
-        { text: "How can i trust you? You look like a weapon", category: "B" },
-        { text: "ponders for a moment", category: "C" }
+        { text: "I trust him with my life", category: "T" },
+        { text: "How can i trust you? You look like a weapon", category: "F" },
+        { text: "ponders for a moment", category: "N" }
       ],
     },
     {
       question: "You decided to follow King George to his kingdom. However, as you walk along, you heard a loud yell for help. You moved closer, and all you saw was a kingdom engulfed in flames.",
       answers: [
-        { text: "how can fire burn underwater?!", category: "A" },
-        { text: "the fishmen are burning!!! i need to do something to save them!", category: "B" },
-        { text: "maybe i should not have gone with King George…", category: "C" }
+        { text: "how can fire burn underwater?!", category: "S" },
+        { text: "the fishmen are burning!!! i need to do something to save them!", category: "P" },
+        { text: "maybe i should not have gone with King George…", category: "J" }
       ],
     },
     {
       question: "The kingdom is under attack! You look to your right, and see a large, fire-breathing dragon. To your left, King George is gearing up for a fight. “Young one, I cannot defeat this monster alone. Only with your help can we save my kingdom.” He offers you a range of weapons. Which will you choose?",
       answers: [
-        { text: "A sea shell grenade", category: "A" },
-        { text: "A beautiful pearl dagger", category: "B" },
-        { text: "A sturdy bow and arrow made of an unknown substance", category: "C" }
+        { text: "A sea shell grenade", category: "S" },
+        { text: "A beautiful pearl dagger", category: "T" },
+        { text: "A sturdy bow and arrow made of an unknown substance", category: "P" }
       ],
     },
     {
       question: "HAIYAAAAA” With all your might, you and King George charge towards the dragon. In a flurry of activity, the dragon was somehow defeated. The kingdom is safe! You turn around, expecting to see a desolate wasteland. However, the kingdom has beem magically restored. In fact, it was like the fight never happened at all…",
       answers: [
-        { text: "How?? What??", category: "A" },
-        { text: "Its beautiful… can I post this on Instagram?", category: "B" },
-        { text: "im overstimulated… i need to sleep now..", category: "C" }
+        { text: "How?? What??", category: "J" },
+        { text: "Its beautiful… can I post this on Instagram?", category: "E" },
+        { text: "im overstimulated… i need to sleep now..", category: "I" }
       ],
     },
     {
       question: "“Thank you for your service. I could not have saved my people without you.” You spend a week in the kingdom, enjoying the lavish wonders of the underwater kingdom. After a week, you decide it’s time to return home. The king offers you a ride home, along with a gift of your choice. What will you choose?",
       answers: [
-        { text: "The weapon you fought the dragon with, along with a shell you can only find in the kingdom", category: "A" },
-        { text: "A treasure chest with golden coins", category: "B" },
-        { text: "An invitation to the kings birthday party next year", category: "C" }
+        { text: "The weapon you fought the dragon with, along with a shell you can only find in the kingdom", category: "F" },
+        { text: "A treasure chest with golden coins", category: "T" },
+        { text: "An invitation to the kings birthday party next year", category: "E" }
       ],
     },
     {
       question: "You enter a humongous bubble and found yourself magically transported back home. What a journey it has been! What the first thing you will do?",
       answers: [
-        { text: "sleep for the next 48 hours", category: "A" },
-        { text: "call my friends to tell them about my crazy adventure", category: "B" },
-        { text: "reflect on life and its meaning", category: "C" }
+        { text: "sleep for the next 48 hours", category: "I" },
+        { text: "call my friends to tell them about my crazy adventure", category: "E" },
+        { text: "reflect on life and its meaning", category: "N" }
       ],
     },
 
