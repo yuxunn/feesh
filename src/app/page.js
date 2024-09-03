6c6de38900cf70e7bba0616b52b0fb6ecd4420d3
taugwa <e0967895@u.nus.edu>
2024-09-03 22:42:59 +0800

    updated some frontend stuff n added pics until q8

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,21 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-blue-100">
-      <div className="container max-w-lg mx-auto text-center p-8 bg-white shadow-lg rounded-lg">
-        <h1 className="text-4xl font-bold mb-8 text-blue-600">Quiz App</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-[url('/static/Background.png')] bg-cover bg-center font-sans bg-no-repeat p-4">
+      <div className="container max-w-md  mx-auto text-center p-8 border-2 border-black bg-white shadow-lg rounded-lg">
+        <div className=''>
+          <img src="static/test_image.jpg" alt="Story" className="object-cover rounded-md mb-4 border-2 border-black rounded" />
+        </div>
+        <h1 className="py-2 text-3xl mx-2 font-bold mb-8 text-[#5359FF]">Which marine creature are you?</h1>
         <Link href="/quiz">
-          <button className="bg-blue-500 text-black font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">
+          <button className="bg-[#FFB8B8] text-black w-[12em] h-[3em] text-l py-2 px-4 border-2 border-black rounded-lg hover:bg-[#DF9A9A] transition duration-300">
             Start Quiz
           </button>
         </Link>
+      </div>
+      <div className='text-black p-4 pt-8 text-center text-sm'>
+        <p>Made by @jing.yunn and @yuxun3696 :)</p> 
+        <p>github repo link</p>
       </div>
     </main>
   );
