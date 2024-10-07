28195633edec3bb9160d4691513c018b7f484a30
taugwa <e0967895@u.nus.edu>
2024-10-07 17:02:49 +0800

    added personality deets

--- src/app/result/data.js
+++ src/app/result/data.js
@@ -1,129 +1,139 @@
 export const animalData = {
     Clownfish: {
       writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
+        'the life of the party',
+        '*pokes you for fun and then pretends nothing happened*',
+        'accidentally double-books their time',
+        'fluent in yappanese'
       ],
       bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
-      ],
-      compatible: ['Octopus', 'Tortoise'],
-      incompatible: ['Shark', 'Axolotl'],
-      image: '/static/characters/feesh.png'
-    },
-    Tortoise: {
-      writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
-      ],
-      bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
+        'Creative & energetic',
+        'Highly social',
+        'Loves attention',
+        'A clown'
       ],
       compatible: ['Axolotl', 'Whale'],
+      incompatible: ['Turtle', 'Pufferfish'],
+      image: '/static/characters/feesh.png'
+    },
+    Turtle: {
+      writeUp: [
+        '\"i can do it !!!….???\"',
+        'dislikes tension and conflict',
+        '\"“when life gives me lemons try ur best to eat the lemon\”',
+        '*overstimulated after 1 night out*'
+      ],
+      bulletPoints: [
+        'Soft but mighty',
+        'Dependable',
+        'Rich inner world',
+        'Prone to stress'
+      ],
+      compatible: ['Jellyfish', 'Whale'],
       incompatible: ['Clownfish', 'Octopus'],
       image: '/static/characters/tortoise.png'
     },
     Jellyfish: {
       writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
+        '\"life is a journey n i am d destination ~\"',
+        'very supportive friends',
+        '\"anything u choose\"',
+        '*spends 20 mins flipping the menu*'
       ],
       bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
+        'Easygoing & creative',
+        'Empathetic',
+        'Highly tolerant',
+        'Indecisive'
       ],
-      compatible: ['Shark', 'Whale'],
-      incompatible: ['Clownfish', 'Octopus'],
+      compatible: ['Turtle', 'Axolotl'],
+      incompatible: ['Shark', 'Pufferfish'],
       image: '/static/characters/jellyfish.png'
     },
     Octopus: {
       writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
+        'always one step ahead',
+        '\"“let\'s bet $5 on this heh heh\”',
+        '\“sure but—\”',
+        'likes a good challenge'
       ],
       bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
+        'Intelligent & curious',
+        'Quick-witted',
+        'Likes intellectual stimulation',
+        'Argumentative'
       ],
-      compatible: ['Clownfish', 'Jellyfish'],
-      incompatible: ['Tortoise', 'Axolotl'],
+      compatible: ['Shark', 'Pufferfish'],
+      incompatible: ['Turtle', 'Whale'],
       image: '/static/characters/octopuz.png'
     },
     Shark: {
       writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
+        'has an excellent productivity system',
+        '*takes charge of the vacation planning*',
+        'my way or the highway',
+        '\“ok everyone lez do this\”'
       ],
       bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
+        'Assertive and driven',
+        'Committed',
+        'Strong-willed',
+        'A natural leader'
       ],
-      compatible: ['Jellyfish', 'Whale'],
-      incompatible: ['Clownfish', 'Axolotl'],
+      compatible: ['Octopus', 'Pufferfish'],
+      incompatible: ['Jellyfish', 'Axolotl'],
       image: '/static/characters/shark.png'
     },
     Axolotl: {
       writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
+        '*comes up with the most out-of-the-box ideas that still makes sense*',
+        'has 10000 hobbies and/or interests',
+        'constantly seeking to grow',
+        '(is it self-reflection or overthinking?)'
       ],
       bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
+        'Enthusiastic & friendly',
+        'Has intriguing views',
+        'Deep thinker',
+        'Prone to perfectionism'
       ],
-      compatible: ['Tortoise', 'Jellyfish'],
-      incompatible: ['Shark', 'Octopus'],
+      compatible: ['Clownfish', 'Jellyfish'],
+      incompatible: ['Shark', 'Whale'],
       image: '/static/characters/axolotl.png'
     },
     Whale: {
       writeUp: [
-        'writeuppoint1',
-        'writeuppoint2',
-        'writeuppoint3',
-        'writeuppoint4'
+        'may be 20 but has the mental age of 50',
+        '\“back in my day...\”',
+        'gives the best advice',
+        '\"guys what is rizz\”'
       ],
       bulletPoints: [
-        'bulletpoint1',
-        'bulletpoint2',
-        'bulletpoint3',
-        'bulletpoint4',
-        'bulletpoint5'
+        'Steady & reliable',
+        'An old soul',
+        'Methodical',
+        'Resistant to change'
       ],
-      compatible: ['Tortoise', 'Jellyfish'],
-      incompatible: ['Axolotl', 'Clownfish'],
+      compatible: ['Clownfish', 'Turtle'],
+      incompatible: ['Axolotl', 'Octopus'],
       image: '/static/characters/whale.png'
+    },
+    Pufferfish: {
+      writeUp: [
+        '\“help i\'m so busy...\” *signs up for a new commitment*',
+        'chugs 4 coffees/day, sleeps at 3am',
+        '\“wtf is wrong with this?!\”',
+        '\“im bored\” (has a full-time job and 3 coffee chats and a side hustle lined up the next day)'
+      ],
+      bulletPoints: [
+        'Creative & spontaneous ',
+        'Risk-taker',
+        'Always busy',
+        'Defensive'
+      ],
+      compatible: ['Octopus', 'Shark'],
+      incompatible: ['Clownfish', 'Jellyfish'],
+      image: '/static/characters/pufferfish.png'
     }
   };
   
--- src/app/result/page.js
+++ src/app/result/page.js
@@ -2,6 +2,7 @@
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 import Card from './card.jsx';
+import { useRouter } from 'next/navigation';
 
 const ResultContent = () => {
   const searchParams = useSearchParams();
@@ -9,22 +10,45 @@ const ResultContent = () => {
 
   return <Card animal={animal} />;
 };
-
 export default function Results() {
+  const router = useRouter(); // Move useRouter() here
+
   return (
     <div className="font-sans min-h-screen flex flex-col items-center justify-center bg-[url('/static/Background.png')] bg-cover bg-center bg-no-repeat">
       <Suspense fallback={<div>Loading...</div>}>
-        <ResultContent /> 
+        <div className="quiz-container text-black p-4">
+          <ResultContent /> 
+        </div>
+        
       </Suspense>
 
-      <div className="text-black p-1 text-center text-sm">
+      <div className="text-black text-center text-sm">
+        <div className="share mt-2 text-center">
+          <p className="font-bold font-black">SHARE YOUR RESULTS!</p>
+          <div className="flex justify-center space-x-4 mt-2">
+            <img src="/static/socials/instagram.png" alt="Instagram" className="w-8 h-8" />
+            <img src="/static/socials/telegram.png" alt="Telegram" className="w-8 h-8" />
+            <img src="/static/socials/twitter.png" alt="Twitter" className="w-8 h-8" />
+            <img src="/static/socials/whatsapp.png" alt="Whatsapp" className="w-8 h-8" />
+          </div>
+          <div className="pb-4">
+            <button
+              onClick={() => router.push('/')} // Use router here
+              className="bg-[#FFB8B8] text-black w-[12em] h-[3em] text-l py-2 px-4 border-2 border-black rounded-lg hover:bg-[#DF9A9A] transition duration-300 mt-6"
+            >
+              Restart
+            </button>
+          </div>
+
+        </div>
+        <div className='text-black p-4 text-center text-sm'>
         <p>Made by @jing.yunn and @yuxun3696 :)</p> 
         <a href="https://github.com/yuxunn/feesh" className="flex justify-center items-center space-x-2">
           <img src="/static/socials/github.png" alt="GitHub" className="w-6 h-6" />
           <span>GitHub</span>
-        </a>  
+        </a>
+      </div> 
       </div>
     </div>
   );
 }
-
